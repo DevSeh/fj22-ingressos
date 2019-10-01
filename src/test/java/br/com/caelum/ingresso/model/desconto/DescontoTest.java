c2df9281ce62292faa2a024f60a2e0d50b1bf030
Sergio <break.seh@gmail.com>
2019-09-30 22:26:41 -0300

    exercicio 3.4

---
+++ src/test/java/br/com/caelum/ingresso/model/desconto/DescontoTest.java
@@ -0,0 +1,25 @@
+package br.com.caelum.ingresso.model.desconto;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import junit.framework.Assert;
+
+public class DescontoTest {
+	@Test
+	public void naoDeveConcederDescontoParaIngressoNormal() {
+		Sala sala = new Sala("Elprateado",new BigDecimal("20.5"));
+		Filme filme = new Filme ("Rogue roll",Duration.ofMinutes(120),"SCIcia",new BigDecimal("12"));
+		Sessao sessao = new Sessao (LocalTime.parse("10:00:00"),filme,sala);
+		Ingresso ingresso = new Ingresso (sessao, new SemDesconto());
+		BigDecimal precoEsperado = new BigDecimal("32.50");
+		Assert.assertEquals(precoEsperado,ingresso.getPreco());
+		
+	}
+ }
